00cf3f89b62a244d3a63afcf6908c318d6f6c396
Lewis-Marshall <gamersworkhouse@gmail.com>
2022-07-16 17:06:05 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import { createSignal, onCleanup, Component } from "solid-js";
+import { render } from "solid-js/web";
+import adapter from "webrtc-adapter";
+
+const App: Component = () => {
+  const [count, setCount] = createSignal(0),
+    timer = setInterval(() => setCount((c) => c + 1), 1000);
+  onCleanup(() => clearInterval(timer));
+
+  return <div>{count()}</div>;
+};
+
+render(() => <App />, document.getElementById("app"));
